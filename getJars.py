43660d0d13748dfb7d931a85f7decb312c840ef3
Jack Lund <jack@m2mconnectionsinc.com>
2013-08-19 21:26:12 -0500

    Added documentation

--- getJars.py
+++ getJars.py
@@ -1,7 +1,17 @@
 import os, sys, subprocess, tempfile
 from jar import Jar
 
+# Script which searches an executable jar file and creates links for all of the
+# dependencies defined in the classpath pointing to the jar files in the local
+# Maven repository. If you pass in the '-r' flag, it will also look for a pom.xml
+# file in the jar and download the dependencies defined in that file to the local
+# Maven repository.
+#
+# Thus, to set up an executable jar to run, just run 'python getJars.py -r jar-file',
+# and then just run it using 'java -jar jar-file'.
+
 def getPom(jar):
+  """Find the pom.xml file in the jar file"""
   poms = jar.findEntries('.*/pom.xml$')
   if len(poms) > 1:
     raise 'More than one pom file found'
@@ -16,6 +26,7 @@ def getPom(jar):
   return name
 
 def resolve(name):
+  """Resolve the jar file dependencies using the passed-in pom file"""
   print 'Resolving jars from pom.xml'
   retcode = subprocess.Popen('mvn -f ' + name + ' dependency:resolve', shell=True,
       stderr=subprocess.STDOUT, stdout=subprocess.PIPE).communicate()[0]
@@ -26,9 +37,11 @@ def resolve(name):
   os.remove(name)
 
 def getJarList(jar):
+  """Retrieve a list of the dependencies required on the classpath"""
   return jar.readManifest()['Class-Path']
 
 def makeSymlinks(names):
+  """Make the symbolic links pointing to the jar files needed"""
   names = [(name, os.path.basename(name)) for name in names.split(' ') if not os.path.exists(name)]
   for root, dirs, files in os.walk(os.path.join(os.environ['HOME'], '.m2', 'repository')):
     for name in names:
